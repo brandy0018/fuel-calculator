fdaa1401054ca9f5b9deda0ec3e2118c10dd4234
brandy0018 <stefan.brandstaetter@gmx.net>
2026-09-06 02:04:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='fuelcalc-github-responsive-v2';
+const CACHE_NAME='fuelcalc-v1.8.0';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest'];
 
 self.addEventListener('install',event=>{
