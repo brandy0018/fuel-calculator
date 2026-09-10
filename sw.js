e46a5b82a6ccdf755eaadc5fb472b253be747f55
brandy0018 <stefan.brandstaetter@gmx.net>
2026-09-10 19:44:42 +0200

    Bump service worker cache to v1.10.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='fuelcalc-v1.10.0';
+const CACHE_NAME='fuelcalc-v1.10.1';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest'];
 
 self.addEventListener('install',event=>{
