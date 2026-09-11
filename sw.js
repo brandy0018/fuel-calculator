a898330630cef18bda5c61f5052173aead813620
brandy0018 <stefan.brandstaetter@gmx.net>
2026-09-11 23:22:40 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='fuelcalc-v1.18.5';
+const CACHE_NAME='fuelcalc-v1.19.4';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest'];
 
 self.addEventListener('install',event=>{
